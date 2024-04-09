e8b12ee4e3931f480e56efc4f42e1d106f95c215
pleumann <joerg.pleumann@gmail.com>
2024-04-09 11:33:24 +0200

    Adding starter script

---
+++ aoc.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+if [ "$1" == "" ]
+then
+  echo "Usage: ./aoc23.sh <day> [<input>]"
+  exit
+fi
+ 
+if [ "$2" == "" ]
+then
+  INPUT="input.txt"
+else
+  INPUT="$2"
+fi
+
+java -cp dist/AdventOfCode23.jar day${1}.Puzzle src/day${1}/${INPUT}
