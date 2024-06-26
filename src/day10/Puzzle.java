83c671c1f11e003766329fad4300baeefd750845
pleumann <joerg.pleumann@gmail.com>
2024-06-26 11:14:06 +0200

    Slightly more concise version of day 10 (now uses flood-fill also for walking the path)

--- src/day10/Puzzle.java
+++ src/day10/Puzzle.java
@@ -15,7 +15,7 @@ public class Puzzle {
     char[][] map;
     
     /**
-     * The width/height the (square) map.
+     * The width/height of the (square) map.
      */
     int size;
 
@@ -27,7 +27,7 @@ public class Puzzle {
     /**
      * The starting column of the loop.
      */
-    int startCol;
+    int startColumn;
 
     /**
      * Dumps the map.
@@ -53,70 +53,27 @@ public class Puzzle {
             int j = s.indexOf('S');
             if (j != -1) {
                 startRow = i;
-                startCol = j;
+                startColumn = j;
             }
             
             s = "." + rd.readLine() + ".";
         }
         map[size - 1] = (".".repeat(size)).toCharArray();
     }
-    
-    /**
-     * Fixes the start position of the loop so it is a pipe symbol.
-     */
-    void fixStart() {
-        boolean up    = "|7F".indexOf(map[startRow - 1][startCol]) != -1;
-        boolean down  = "|LJ".indexOf(map[startRow + 1][startCol]) != -1;
-        boolean left  = "-LF".indexOf(map[startRow][startCol - 1]) != -1;
-        boolean right = "-7J".indexOf(map[startRow][startCol + 1]) != -1;
-        
-        char c = 'S';
-        if (up && down) {
-            c = '|';
-        } else if (left && right) {
-            c = '-';
-        } else if (up && right) {
-            c = 'L';
-        } else if (up && left) {
-            c = 'J';
-        } else if (down && left) {
-            c = '7';
-        } else if (down && right) {
-            c = 'F';
-        } else {
-            throw new RuntimeException("WTF!?");
-        }
-        
-        map[startRow][startCol] = c;
-    }
-
         
     /**
      * Inflates a single character of the original map to 3x3 characters.
      */
-    char[][] inflate(char c) {
+    String[] inflate(char c) {
         return switch (c) {
-            case '|' -> new char[][] { { '.', '|', '.' }, 
-                                       { '.', '|', '.' },
-                                       { '.', '|', '.' } };
-            case '-' -> new char[][] { { '.', '.', '.' }, 
-                                       { '-', '-', '-' },
-                                       { '.', '.', '.' } };
-            case '7' -> new char[][] { { '.', '.', '.' }, 
-                                       { '-', '7', '.' },
-                                       { '.', '|', '.' } };
-            case 'F' -> new char[][] { { '.', '.', '.' }, 
-                                       { '.', 'F', '-' },
-                                       { '.', '|', '.' } };
-            case 'J' -> new char[][] { { '.', '|', '.' }, 
-                                       { '-', 'J', '.' },
-                                       { '.', '.', '.' } };
-            case 'L' -> new char[][] { { '.', '|', '.' }, 
-                                       { '.', 'L', '-' },
-                                       { '.', '.', '.' } };
-            default  -> new char[][] { { '.', '.', '.' }, 
-                                       { '.', '.', '.' },
-                                       { '.', '.', '.' } };
+            case '|' -> new String[] { ".*.", ".*.", ".*." };
+            case '-' -> new String[] { "...", "***", "..." };
+            case '7' -> new String[] { "...", "**.", ".*." };
+            case 'F' -> new String[] { "...", ".**", ".*." };
+            case 'J' -> new String[] { ".*.", "**.", "..." };
+            case 'L' -> new String[] { ".*.", ".**", "..." };
+            case 'S' -> new String[] { ".*.", "***", ".*." };
+            default  -> new String[] { "...", "...", "..." };
         };
     }
     
@@ -129,10 +86,10 @@ public class Puzzle {
             for (int j = 0; j < size ; j++) {
                 char c = map[i][j];
                 
-                char[][] d = Puzzle.this.inflate(c);
+                String[] s = inflate(c);
                 
                 for (int k =0; k < 3; k++) {
-                    System.arraycopy(d[k], 0, newMap[3 * i + k], 3 * j, 3);
+                    System.arraycopy(s[k].toCharArray(), 0, newMap[3 * i + k], 3 * j, 3);
                 }
             }
         }
@@ -140,49 +97,29 @@ public class Puzzle {
         map = newMap;
         size = 3 * size;
         startRow = 3 * startRow + 1;
-        startCol = 3 * startCol + 1;
-    }
-
-    /**
-     * Recursively walks the path, fills it with '#', returns count.
-     */
-    int walk(int row, int col) {
-        char c = map[row][col];
-        
-        if (c == '#') {
-            return 0;
-        }
-        
-        map[row][col] = '#';
-        
-        return switch (c) {
-            case '|' -> 1 + walk(row - 1, col) + walk(row + 1, col);
-            case '-' -> 1 + walk(row, col - 1) + walk(row, col + 1);
-            case 'L' -> 1 + walk(row - 1, col) + walk(row, col + 1);
-            case '7' -> 1 + walk(row, col - 1) + walk(row + 1, col);
-            case 'J' -> 1 + walk(row - 1, col) + walk(row, col - 1);
-            case 'F' -> 1 + walk(row, col + 1) + walk(row + 1, col);
-            default  -> throw new RuntimeException("WTF!?");
-        };
+        startColumn = 3 * startColumn + 1;
     }
     
     /**
-     * Performs a recursive flood-fill with 'O', returns count.
+     * Performs a recursive flood-fill with given fill and stop characters.
      */
-    int flood(int x, int y) {
+    void flood(int x, int y, char fill, char stop) {
         if (x < 0 || y < 0 || x == size || y == size) {
-            return 0;
+            return;
         }
         
         char c = map[x][y];
         
-        if (c == '#' || c == 'O') {
-            return 0;
+        if (c == fill || c == stop) {
+            return;
         }
                 
-        map[x][y] = 'O';
+        map[x][y] = fill;
         
-        return  1 + flood(x - 1, y) + flood(x + 1, y) + flood(x, y - 1) + flood(x, y + 1);
+        flood(x - 1, y, fill, stop);
+        flood(x + 1, y, fill, stop);
+        flood(x, y - 1, fill, stop);
+        flood(x, y + 1, fill, stop);
     }
     
     /**
@@ -190,11 +127,10 @@ public class Puzzle {
      */
     void solve(BufferedReader r) throws IOException {
         load(r);
-        fixStart();
         dump();
 
         System.out.println("Map size is " + size + " x " + size + ".");
-        System.out.println("Starting position is (" + startRow + ", " + startCol + ").");
+        System.out.println("Starting position is (" + startRow + ", " + startColumn + ").");
         System.out.println();        
 
         System.out.println("Inflating map...");
@@ -203,23 +139,20 @@ public class Puzzle {
         inflate();
         dump();
 
-        System.out.println("Walking loop...");
+        System.out.println("Filling loop...");
         System.out.println();
         
-        int part1 = walk(startRow, startCol);
+        flood(startRow, startColumn, '#', '.');
         dump();
         
-        System.out.println("Flood-filling...");
+        System.out.println("Filling outside...");
         System.out.println();
         
-        int part2 = size * size - part1 - flood(0,0);
+        flood(0, 0, 'O', '#');
         dump();
 
-//        System.out.println("Part 1: " + part1 / 3 / 2);
-//        System.out.println("Part 2: " + part2 / 9);
-        
-        part1 = 0;
-        part2 = 0;
+        int part1 = 0;
+        int part2 = 0;
         for (int i = 1; i < size; i += 3) {
             for (int j = 1; j < size; j += 3) {
                 char c = map[i][j];
